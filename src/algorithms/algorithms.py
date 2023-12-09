e9798ea593a57dd70c930ae214a59bafe489df71
Karkos82822 <119781909+Karkos82822@users.noreply.github.com>
2023-12-09 16:14:02 +0100

    adding file for algorithms

---
+++ src/algorithms/algorithms.py
@@ -0,0 +1,31 @@
+def V_T():
+
+    return
+
+def I_off():
+
+    return
+
+def I_on():
+
+    return
+    
+def subthreshold_swing_slope():
+
+    return
+
+def DIBL():
+
+    return
+
+def g_m():
+
+    return
+
+def g_ds():
+
+    return
+
+def lambda():
+
+    return
