cb04b3595686c15bbcb44337bc7c77b2dc0ba9a7
MaciejMikulski <maciej.mikulski12@gmail.com>
2023-12-17 16:24:23 +0100

    Created files for display widhets.
    Created files for display widgets and reorgainsed main GUI file by providing skeleton code for placement of display widgets.

--- src/GUI/gui_main.py
+++ src/GUI/gui_main.py
@@ -1,11 +1,28 @@
 from tkinter import *
+from tkinter import ttk
 from menu_bar import *
 
 root = Tk()
+root.title("ZPB FET")
 
+# Add menu bar to the program window
 menu = menu_bar(root=root)
-tmp = menu.get_menu_bar()
+root.config(menu=menu.get_menu_bar())
 
-root.config(menu=tmp)
+# Create paned window for display widgets
+pw = PanedWindow(orient='horizontal')
+# Insert display widgets into paned window
+# Graph window
+
+pw.add()
+
+# Parameters display
+
+pw.add()
+
+pw.pack(fill=BOTH, expand=True)
+pw.configure(sashrelief=RAISED)
+
+
 
 root.mainloop()
